8a64826df73450ad66e4b9829e5f14f8540a1b8a
Taylor Langlois <Taylor.Langlois@vu.com>
2023-02-08 10:44:41 -0600

    Added input for rate and total pay line with float values to get numerical print solution

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,12 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = float(input("Enter Hours:"))
+    rate = float(input("Enter Rate:"))
+    total_pay = hrs * rate
+    print(total_pay)
+
+
 
     # end assignment
 
